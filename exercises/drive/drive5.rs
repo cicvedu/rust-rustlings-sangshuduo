1203813e35b3dbb17902471da4971d19b60a302b
Shuduo Sang <sangshuduo@gmail.com>
2023-08-13 00:32:38 +0800

    fix: drive5

--- exercises/drive/drive5.rs
+++ exercises/drive/drive5.rs
@@ -5,13 +5,12 @@
 // You should not modify any existing code. All you need to do is add two line of attributes.
 
 
-// I AM NOT DONE
 
+#[no_mangle]
+pub extern "C"    fn my_demo_function(a:u32) -> u32 {a}
 
-extern {
-    fn my_demo_function(a:u32) -> u32;
-    fn my_demo_function_alias(a:u32) -> u32;
-}
+#[no_mangle]
+pub extern "C"    fn my_demo_function_alias(a:u32) -> u32 {a}
 
 
 
