1dcb059e3402336fec5d0b6f63af7beaa1220b2d
Shuduo Sang <sangshuduo@gmail.com>
2023-08-13 00:24:07 +0800

    fix: drive4

--- exercises/drive/build.rs
+++ exercises/drive/build.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 fn main() {
+    println!("cargo:rustc-cfg=feature=\"pass\"");
     let timestamp = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
 
     // Set TEST_FOO environment variable within the expected range
